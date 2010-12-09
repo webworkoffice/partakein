5824b7f59ac24a1970f4b36803aec18946984f61
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2010-12-09 09:53:04 +0000

    added OpenID deletion interface.

--- trunk/Partake/src/main/java/in/partake/model/dao/IOpenIDLinkageAccess.java
+++ trunk/Partake/src/main/java/in/partake/model/dao/IOpenIDLinkageAccess.java
@@ -3,8 +3,7 @@ package in.partake.model.dao;
 
 public interface IOpenIDLinkageAccess {
 
-    public abstract void addOpenID(PartakeConnection con, String identity, String userId) throws DAOException;
-
-    public abstract String getUserId(PartakeConnection con, String identity) throws DAOException;
-
+    public abstract void addOpenID(PartakeConnection con, String identifier, String userId) throws DAOException;
+    public abstract String getUserId(PartakeConnection con, String identifier) throws DAOException;
+    public abstract void removeOpenID(PartakeConnection con, String identifier, String userId) throws DAOException;
 }
