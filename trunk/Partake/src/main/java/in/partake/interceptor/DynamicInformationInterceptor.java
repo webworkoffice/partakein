330e42059a7a2daa085028c2472a909c974281b1
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2010-12-13 13:32:38 +0000

    the same page will be displayed after login.

--- trunk/Partake/src/main/java/in/partake/interceptor/DynamicInformationInterceptor.java
+++ trunk/Partake/src/main/java/in/partake/interceptor/DynamicInformationInterceptor.java
@@ -1,8 +1,10 @@
 package in.partake.interceptor;
 
+import org.apache.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
 
 import in.partake.controller.PartakeActionSupport;
+import in.partake.resource.Constants;
 
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
@@ -11,14 +13,20 @@ public class DynamicInformationInterceptor extends AbstractInterceptor {
 	
 	/** */
 	private static final long serialVersionUID = 1L;
+    private static final Logger logger = Logger.getLogger(DynamicInformationInterceptor.class);
 
+    
 	@Override
 	public String intercept(ActionInvocation invocation) throws Exception {
 
 		Object action = invocation.getAction();
 		if (action instanceof PartakeActionSupport) {
-			PartakeActionSupport pas = (PartakeActionSupport) action;			
-			pas.setCurrentURL(ServletActionContext.getRequest().getRequestURL().toString());
+			PartakeActionSupport pas = (PartakeActionSupport) action;
+			String currentURL = ServletActionContext.getRequest().getRequestURL().toString(); 
+			pas.setCurrentURL(currentURL);
+			ServletActionContext.getRequest().setAttribute(Constants.ATTR_CURRENT_URL, currentURL);
+		} else {
+			logger.warn("action is not extended from PartakeActionSupport");
 		}
 		
 		return invocation.invoke();
