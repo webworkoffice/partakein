c9bc15d710f905df7c6355c07e3a272ec3e2e681
skypencil <skypencil@35327b65-f2d5-03ad-2307-039ebe4172a5>
2010-12-23 03:34:38 +0000

    Change to all events start at o'clock by default. The end-time and deadline start too.

--- trunk/Partake/src/test/java/in/partake/controller/EventsEditControllerTest.java
+++ trunk/Partake/src/test/java/in/partake/controller/EventsEditControllerTest.java
@@ -2,6 +2,7 @@ package in.partake.controller;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -19,13 +20,28 @@ public final class EventsEditControllerTest {
 	 * </ul>
 	 * 
 	 * @author skypencil (@eller86)
+	 * @throws InterruptedException 
 	 */
 	@Test
-	public void editNewTest() {
+	public void editNewTest() throws InterruptedException {
+		try {
+			editNewTestInner();
+
+			// 現在時刻に依存するテストケースであるため、少なくとも分（minute）が異なる2点でテストする必要がある
+			TimeUnit.SECONDS.sleep(70);
+			editNewTestInner();
+		} catch (InterruptedException e) {
+			// we cannot ignore this exception
+			// because this exception means that our testcase didn't run twice.
+			throw e;
+		}
+	}
+	
+	private void editNewTestInner() {
 		EventsEditController controller = null;
 		Date oneDayAfter = null;
 		final Calendar oneDayAfterCalendar = Calendar.getInstance();
-		for (int i = 0; i < 20 && (controller == null || oneDayAfterCalendar.get(Calendar.MINUTE) != controller.getSmin()); ++i) {
+		for (int i = 0; i < 20 && (controller == null || oneDayAfterCalendar.get(Calendar.HOUR_OF_DAY) != controller.getShour()); ++i) {
 			oneDayAfter = new Date(new Date().getTime() + 1000 * 3600 * 24);
 
 			// ここで他のプロセスにCPU取られると、Controller内部のoneDayAfterとここで宣言したローカル変数のoneDayAfterが
@@ -43,20 +59,20 @@ public final class EventsEditControllerTest {
 		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.MONTH) + 1, controller.getSmonth());
 		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.DAY_OF_MONTH), controller.getSday());
 		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.HOUR_OF_DAY), controller.getShour());
-		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.MINUTE), controller.getSmin());
+		Assert.assertEquals(0, controller.getSmin());	// 2回繰り返すことで、ここがたまたま通ってしまう可能性を排除
 
 		// End Date
 		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.YEAR), controller.getEyear());
 		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.MONTH) + 1, controller.getEmonth());
 		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.DAY_OF_MONTH), controller.getEday());
 		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.HOUR_OF_DAY), controller.getEhour());
-		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.MINUTE), controller.getEmin());
+		Assert.assertEquals(0, controller.getEmin());
 
 		// Deadline
 		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.YEAR), controller.getDyear());
 		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.MONTH) + 1, controller.getDmonth());
 		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.DAY_OF_MONTH), controller.getDday());
 		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.HOUR_OF_DAY), controller.getDhour());
-		Assert.assertEquals(oneDayAfterCalendar.get(Calendar.MINUTE), controller.getDmin());
+		Assert.assertEquals(0, controller.getDmin());
 	}
 }
