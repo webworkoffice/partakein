8cfe1dbacd64afda6e03d0f69577d88084110c82
skypencil <skypencil@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-01-12 01:31:07 +0000

    Created test case for DirectMessageCassandraDao. This test's report is red. This result tells us this DAO gets any envelope. Why?

---
+++ trunk/Partake/src/test/java/in/partake/model/dao/cassandra/DirectMessageCassandraDaoTest.java
@@ -0,0 +1,91 @@
+package in.partake.model.dao.cassandra;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import in.partake.model.dao.DAOException;
+import in.partake.model.dao.DataIterator;
+import in.partake.model.dto.DirectMessage;
+import in.partake.model.dto.DirectMessageEnvelope;
+import in.partake.model.dto.DirectMessagePostingType;
+import in.partake.resource.PartakeProperties;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+/**
+ * @see DirectMessageCassandraDao
+ * @author skypencil(@eller86)
+ */
+// FIXME in.partake.mode=test で実行できない。というかMockのテストをやりたいわけではないしなぁ。
+public class DirectMessageCassandraDaoTest {
+	private static final String TWITTER_ID = "eller86";
+	private static final String MESSAGE = "message";
+	private static final String EVENT_OWNER_ID = "owner_id";
+
+	public TestName name = new TestName();
+	private CassandraConnectionPool pool;
+	private DirectMessageCassandraDao dao;
+	private String envelopeId;
+
+	@Before
+	public void setup() throws DAOException {
+		if (!PartakeProperties.get().getDAOFactoryClassName().endsWith("CassandraDAOFactory")) {
+			Assert.fail("This test doesn't run because properties file isn't ready.");
+		}
+
+		CassandraDAOFactory factory = new CassandraDAOFactory();
+		pool = new CassandraConnectionPool();
+		dao = new DirectMessageCassandraDao(factory);
+
+		CassandraConnection con = pool.getConnection();
+		try {
+			envelopeId = dao.getFreshId(con);
+		} finally {
+			con.invalidate();
+		}
+	}
+
+	@Test
+	public void testSendAndGet() throws DAOException, InterruptedException {
+		Date deadline = new Date();
+
+		{
+			DirectMessage embryo = new DirectMessage(EVENT_OWNER_ID, MESSAGE);
+			CassandraConnection sendCon = pool.getConnection();
+			try {
+				dao.addMessage(sendCon, envelopeId, embryo);
+				dao.sendEnvelope(sendCon, envelopeId, TWITTER_ID, TWITTER_ID, deadline, DirectMessagePostingType.POSTING_TWITTER_DIRECT);
+			} finally {
+				sendCon.invalidate();
+			}
+		}
+
+		TimeUnit.SECONDS.sleep(3);
+
+		{
+			CassandraConnection getCon = pool.getConnection();
+			boolean found = false;
+			try {
+				DataIterator<DirectMessageEnvelope> iter = dao.getEnvelopeIterator(getCon);
+
+				while (iter.hasNext()) {
+					DirectMessageEnvelope envelope = iter.next();
+					Assert.assertNotNull(envelope);
+					Assert.assertNotNull(envelope.getEnvelopeId());
+					Assert.assertNotNull(envelope.getDeadline());
+					if (envelope.getEnvelopeId().equals(envelopeId)) {
+						Assert.assertEquals(deadline, envelope.getDeadline());
+						found = true;
+					}
+				}
+
+				Assert.assertTrue(found);
+			} finally {
+				getCon.invalidate();
+			}
+		}
+	}
+}
