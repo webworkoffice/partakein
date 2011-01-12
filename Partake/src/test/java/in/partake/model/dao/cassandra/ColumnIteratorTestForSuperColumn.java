9641dd686493528665c9ac2f6e20566a307ce465
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-01-12 07:18:51 +0000

    updated ColumnIterator test.

--- Partake/src/test/java/in/partake/model/dao/cassandra/ColumnIteratorTestForSuperColumn.java
+++ Partake/src/test/java/in/partake/model/dao/cassandra/ColumnIteratorTestForSuperColumn.java
@@ -2,9 +2,7 @@ package in.partake.model.dao.cassandra;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 import junit.framework.Assert;
