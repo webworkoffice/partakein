301f15804bbbe4335277a6a2dcdb40bcb998777f
skypencil <skypencil@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-01-12 00:34:45 +0000

    Added assertion to test cases.

--- Partake/src/test/java/in/partake/model/dao/cassandra/EventNotificationStatusTestWithMessageCassandraDao.java
+++ Partake/src/test/java/in/partake/model/dao/cassandra/EventNotificationStatusTestWithMessageCassandraDao.java
@@ -4,6 +4,7 @@ import junit.framework.Assert;
 import in.partake.model.dao.DAOException;
 import in.partake.model.dao.DataIterator;
 import in.partake.model.dto.EventNotificationStatus;
+import in.partake.resource.PartakeProperties;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +14,7 @@ import org.junit.rules.TestName;
  * @see MessageCassandraDao
  * @author skypencil(@eller86)
  */
+//FIXME in.partake.mode=test で実行できない。というかMockのテストをやりたいわけではないしなぁ。
 public class EventNotificationStatusTestWithMessageCassandraDao {
 	public TestName name = new TestName();
 	private MessageCassandraDao dao;
@@ -21,6 +23,10 @@ public class EventNotificationStatusTestWithMessageCassandraDao {
 
 	@Before
 	public void setup() throws DAOException {
+		if (!PartakeProperties.get().getDAOFactoryClassName().endsWith("CassandraDAOFactory")) {
+			Assert.fail("This test doesn't run because properties file isn't ready.");
+		}
+
 		CassandraDAOFactory factory = new CassandraDAOFactory();
 		pool = new CassandraConnectionPool();
 		dao = new MessageCassandraDao(factory);
@@ -70,6 +76,7 @@ public class EventNotificationStatusTestWithMessageCassandraDao {
 		createNotification(eventId);
 
 		CassandraConnection conUpdate = pool.getConnection();
+		boolean found = false;
 		try {
 			DataIterator<EventNotificationStatus> iterator = dao.getNotificationStatuses(conUpdate);
 			while (iterator.hasNext()) {
@@ -86,13 +93,15 @@ public class EventNotificationStatusTestWithMessageCassandraDao {
 					iterator.update(status);
 				}
 			}
+			Assert.assertTrue(found);
 		} finally {
 			conUpdate.invalidate();
 		}
-			
+
 		CassandraConnection conGet = pool.getConnection();
 		try {
 			EventNotificationStatus updatedStatus = dao.getNotificationStatus(conGet, eventId);
+			Assert.assertNotNull(updatedStatus);
 			Assert.assertEquals(eventId, updatedStatus.getEventId());
 			Assert.assertTrue(updatedStatus.isBeforeDeadlineOneday());
 			Assert.assertFalse(updatedStatus.isBeforeDeadlineHalfday());
@@ -108,21 +117,25 @@ public class EventNotificationStatusTestWithMessageCassandraDao {
 
 		{
 			CassandraConnection conUpdate1 = pool.getConnection();
+			boolean found = false;
 			try {
 				for (DataIterator<EventNotificationStatus> iterator = dao.getNotificationStatuses(conUpdate1); iterator.hasNext();) {
 					EventNotificationStatus status = iterator.next();
 					Assert.assertNotNull(status);
-		
+
 					if (eventId.equals(status.getEventId())) {
+						found = true;
+
 						Assert.assertFalse(status.isBeforeDeadlineOneday());
 						Assert.assertFalse(status.isBeforeDeadlineHalfday());
 						Assert.assertFalse(status.isBeforeTheDay());
-		
+
 						status.setBeforeDeadlineOneday(true);
 						status.setBeforeTheDay(true);
 						iterator.update(status);
 					}
 				}
+				Assert.assertTrue(found);
 			} finally {
 				conUpdate1.invalidate();
 			}
@@ -130,12 +143,15 @@ public class EventNotificationStatusTestWithMessageCassandraDao {
 
 		{
 			CassandraConnection conUpdate2 = pool.getConnection();
+			boolean found = false;
 			try {
 				for (DataIterator<EventNotificationStatus> iterator = dao.getNotificationStatuses(conUpdate2); iterator.hasNext();) {
 					EventNotificationStatus status = iterator.next();
 					Assert.assertNotNull(status);
 		
 					if (eventId.equals(status.getEventId())) {
+						found = true;
+
 						Assert.assertTrue(status.isBeforeDeadlineOneday());
 						Assert.assertFalse(status.isBeforeDeadlineHalfday());
 						Assert.assertTrue(status.isBeforeTheDay());
@@ -145,6 +161,7 @@ public class EventNotificationStatusTestWithMessageCassandraDao {
 						iterator.update(status);
 					}
 				}
+				Assert.assertTrue(found);
 			} finally {
 				conUpdate2.invalidate();
 			}
@@ -154,8 +171,9 @@ public class EventNotificationStatusTestWithMessageCassandraDao {
 			CassandraConnection conGet = pool.getConnection();
 			try {
 				EventNotificationStatus updatedStatus = dao.getNotificationStatus(conGet, eventId);
+				Assert.assertNotNull(updatedStatus);
 				Assert.assertEquals(eventId, updatedStatus.getEventId());
-				Assert.assertFalse(updatedStatus.isBeforeDeadlineOneday());
+				Assert.assertFalse(updatedStatus.isBeforeDeadlineOneday());	// TODO たまにここで失敗する→整合性が取れないため=仕様？
 				Assert.assertTrue(updatedStatus.isBeforeDeadlineHalfday());
 				Assert.assertTrue(updatedStatus.isBeforeTheDay());
 			} finally {
@@ -171,17 +189,21 @@ public class EventNotificationStatusTestWithMessageCassandraDao {
 		{
 			CassandraConnection conRemove = pool.getConnection();
 			try {
+				boolean found = false;
 				for (DataIterator<EventNotificationStatus> iterator = dao.getNotificationStatuses(conRemove); iterator.hasNext();) {
 					EventNotificationStatus status = iterator.next();
 					Assert.assertNotNull(status);
 		
 					if (eventId.equals(status.getEventId())) {
+						found = true;
+
 						status.setBeforeDeadlineOneday(true);
 						status.setBeforeTheDay(true);
 						iterator.update(status);	
 						iterator.remove();
 					}
 				}
+				Assert.assertTrue(found);
 			} finally {
 				conRemove.invalidate();
 			}
