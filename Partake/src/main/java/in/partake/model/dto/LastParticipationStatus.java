78c9959bc2de100f73f2ed475793be25abf67e8c
skypencil <skypencil@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-01-24 09:57:06 +0000

    Wrote comment to the LastParticipationStatus class.

--- Partake/src/main/java/in/partake/model/dto/LastParticipationStatus.java
+++ Partake/src/main/java/in/partake/model/dto/LastParticipationStatus.java
@@ -1,8 +1,21 @@
 package in.partake.model.dto;
 
+/**
+ * 最後に行われたParticipationStatusの変更内容を表す
+ */
 public enum LastParticipationStatus {
+	/**
+	 * 他ユーザのキャンセルなどの要因によって、正式な参加者に昇格した
+	 */
     ENROLLED,
+    /**
+     * 正式な参加者から補欠に降格した
+     */
     NOT_ENROLLED,
+    /**
+     * ユーザの意志でステータスが変更された
+     * リマインダを送信する必要はない
+     */
     CHANGED;
     
     private static LastParticipationStatus SAFE_VALUE = CHANGED;
