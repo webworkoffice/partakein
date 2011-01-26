2876d55e2291e449d1650002d93e49cad1c8b7ca
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-01-26 10:52:20 +0000

    implementing JPAURLShortenerDao

--- Partake/src/main/java/in/partake/model/dao/jpa/JPAURLShortenerDao.java
+++ Partake/src/main/java/in/partake/model/dao/jpa/JPAURLShortenerDao.java
@@ -1,32 +1,48 @@
 package in.partake.model.dao.jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 import in.partake.model.dao.DAOException;
 import in.partake.model.dao.IURLShortenerAccess;
 import in.partake.model.dao.PartakeConnection;
+import in.partake.model.dto.ShortenedURLData;
+import in.partake.model.dto.pk.ShortenedURLDataPK;
 
 class JPAURLShortenerDao extends JPADao implements IURLShortenerAccess {
 
     @Override
-    public void addShortenedURL(PartakeConnection con, String originalURL, String type, String shortenedURL) throws DAOException {
-        // TODO Auto-generated method stub
+    public void addShortenedURL(PartakeConnection con, String originalURL, String serviceType, String shortenedURL) throws DAOException {
+        ShortenedURLData data = new ShortenedURLData(originalURL, serviceType, shortenedURL);
         
+        EntityManager em = getEntityManager(con);
+        em.persist(data);
     }
 
     @Override
-    public String getShortenedURL(PartakeConnection con, String originalURL, String type) throws DAOException {
-        // TODO Auto-generated method stub
-        return null;
+    public String getShortenedURL(PartakeConnection con, String originalURL, String serviceType) throws DAOException {
+        EntityManager em = getEntityManager(con);
+        ShortenedURLData data = em.find(ShortenedURLData.class, new ShortenedURLDataPK(originalURL, serviceType));
+        
+        if (data != null) {
+            return data.getShortenedURL();
+        } else {
+            return null;
+        }
     }
 
     @Override
     public String getShortenedURL(PartakeConnection con, String originalURL) throws DAOException {
-        // TODO Auto-generated method stub
-        return null;
+        EntityManager em = getEntityManager(con);
+        Query q = em.createQuery("SELECT surl FROM shortenedurldata surl where originalurl := ?");
+        q.setParameter(1, originalURL);
+        
+        throw new RuntimeException("not implemented yet.");
     }
 
     @Override
     public void truncate(PartakeConnection con) throws DAOException {
         // TODO Auto-generated method stub
-        
+        throw new RuntimeException("not implemented yet.");
     }
 }
---
+++ Partake/src/test/java/in/partake/model/dto/ShortenedURLTest.java
@@ -0,0 +1,32 @@
+package in.partake.model.dto;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ShortenedURLTest {
+    private ShortenedURLData[] samples;
+    
+    @Before
+    public void createSampleData() {
+        samples = new ShortenedURLData[] {
+                new ShortenedURLData(),
+                new ShortenedURLData("http://www.example.com/", "bitly", "http://bit.ly/example"),
+                new ShortenedURLData("http://www.example.com/", "tco",   "http://t.oo/example"),
+        };
+    }
+    
+    @Test
+    public void testCopyConstructor() {
+        for (ShortenedURLData source : samples) {
+            Assert.assertEquals(source, new ShortenedURLData(source));
+        }
+        
+        for (ShortenedURLData lhs : samples) {
+            for (ShortenedURLData rhs : samples) {
+                if (lhs == rhs) { continue; }
+                Assert.assertFalse(lhs.equals(rhs));
+            }
+        }
+    }
+}
