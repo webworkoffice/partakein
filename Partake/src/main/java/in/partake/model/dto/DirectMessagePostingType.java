acbffb0389a237f1b04fcaf92cf581bd8765fbcb
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-01-28 04:28:23 +0000

    adding testcases...

--- Partake/src/main/java/in/partake/model/dto/DirectMessagePostingType.java
+++ Partake/src/main/java/in/partake/model/dto/DirectMessagePostingType.java
@@ -2,5 +2,5 @@ package in.partake.model.dto;
 
 public enum DirectMessagePostingType {
     POSTING_TWITTER_DIRECT, // receiverId へ direct message を送る
-    POSTING_TWITTER,        // senderId が
+    POSTING_TWITTER,        // senderId が message をつぶやく
 }
