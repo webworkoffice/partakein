9bc5e5043169c960129b80931256541fa20ca61f
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-01-31 05:24:16 +0000

    renamed aux to auxiliary because windows eclipse cannot correctly handle a folder named aux.

---
+++ trunk/Partake/src/main/java/in/partake/model/dto/auxiliary/UserPermission.java
@@ -0,0 +1,30 @@
+package in.partake.model.dto.auxiliary;
+
+public enum UserPermission {
+    /** can edit the event */
+    EVENT_EDIT(),
+
+    /** can remove the event */
+    EVENT_REMOVE,
+
+    /** can edit a sub event */
+    EVENT_EDIT_SUBEVENT,
+    
+    /** can retrieve the participation list */
+    EVENT_PARTICIPATION_LIST,
+    
+    /** show the event even if the event is private. */
+    EVENT_PRIVATE_EVENT,
+    
+    /** can send message to participants */
+    EVENT_SEND_MESSAGE,
+    
+    /** remove comment */
+    EVENT_REMOVE_COMMENT,
+    
+    /** edit participants */
+    EVENT_EDIT_PARTICIPANTS,
+    
+    ;
+    
+}
