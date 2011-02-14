768ec10f29675a373c467255e8f4be27112b8cf0
skypencil <skypencil@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-02-14 22:51:37 +0000

    Add unit test for CassandraKeyColumnDataIterator, and fix its bugs.

--- Partake/src/main/java/in/partake/model/dao/cassandra/CassandraKeyColumnDataIterator.java
+++ Partake/src/main/java/in/partake/model/dao/cassandra/CassandraKeyColumnDataIterator.java
@@ -33,8 +33,8 @@ public class CassandraKeyColumnDataIterator<T> extends DataIterator<T> {
     public boolean hasNext() throws DAOException {
         while (true) {
             if (inner == null) {
-                if (outer.hasNext()) { return false; }
-                currentKey = outer.next();
+                if (!outer.hasNext()) { return false; }
+                currentKey = outer.nextWithPrefix();
                 inner = new ColumnIterator(con, desc.keyspace, currentKey, desc.columnFamily, false, desc.readConsistency, desc.writeConsistency);
             }
 
