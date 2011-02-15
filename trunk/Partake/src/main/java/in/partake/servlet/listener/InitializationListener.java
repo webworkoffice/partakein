4d7b94b259ff8ef619beb3777571bc2577347ee8
skypencil <skypencil@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-02-15 05:20:38 +0000

    add meaning-less query to avoid issue 45. we should test more.

--- trunk/Partake/src/main/java/in/partake/servlet/listener/InitializationListener.java
+++ trunk/Partake/src/main/java/in/partake/servlet/listener/InitializationListener.java
@@ -1,6 +1,10 @@
 package in.partake.servlet.listener;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 import in.partake.daemon.TwitterDaemon;
+import in.partake.view.Helper;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -14,6 +18,26 @@ public class InitializationListener implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
 		TwitterDaemon.getInstance().schedule();
+		initializeCssVersion(arg0);
+	}
+
+	private void initializeCssVersion(ServletContextEvent event) {
+		String cssPath = event.getServletContext().getRealPath("css");
+		File cssDir = new File(cssPath);
+		long version = 0L;
+		if (cssDir.exists()) {
+			for (File cssFile : cssDir.listFiles(new FilenameFilter(){
+				@Override
+				public boolean accept(File dir, String name) {
+					return name.endsWith(".css");
+				}
+			})) {
+				version ^= cssFile.lastModified();
+			}
+		} else {
+			logger.warn("cannot find the css/ directory.");
+		}
+		Helper.setCssVersion(Long.toString(version));
 	};
 
 	@Override
