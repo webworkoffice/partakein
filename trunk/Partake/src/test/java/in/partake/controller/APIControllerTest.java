a57bfa0d4714f572ec82a80ca1591a71c69c3ffc
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-02-25 10:46:09 +0000

    adding API...

---
+++ trunk/Partake/src/test/java/in/partake/controller/APIControllerTest.java
@@ -0,0 +1,13 @@
+package in.partake.controller;
+
+import in.partake.resource.PartakeProperties;
+
+import org.apache.struts2.StrutsTestCase;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public class APIControllerTest extends StrutsTestCase {
+
+  
+    
+}
