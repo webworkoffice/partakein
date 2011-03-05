e40e4111088e43e8d566907716b0d0db5cad6464
skypencil <skypencil@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-03-05 03:22:33 +0000

    prepare to create platform-free test cases for Services.

--- Partake/src/test/java/in/partake/service/EventServiceTestCaseBase.java
+++ Partake/src/test/java/in/partake/service/EventServiceTestCaseBase.java
@@ -1,11 +1,23 @@
 package in.partake.service;
 
+import in.partake.model.dto.Event;
+import junit.framework.Assert;
+
 import org.junit.Test;
 
-public class EventServiceTestCaseBase extends PartakeService {
-
+public abstract class EventServiceTestCaseBase extends AbstractServiceTestCaseBase {
     @Test
-    public void testCasesShouldBeAdded() {
-        throw new RuntimeException("Not implemented yet.");
+    public void testToGetEventById() throws Exception {
+        EventService service = EventService.get();
+        String eventId = createRandomId();
+        Assert.assertNull(service.getEventById(eventId));
+
+        Event event = createEvent(eventId);
+        String updatedEventId = service.create(event, null, null);
+        Assert.assertFalse(updatedEventId.equals(eventId));
+
+        Event storedEvent = service.getEventById(updatedEventId);
+        Assert.assertNotNull(storedEvent);
+        Assert.assertEquals(updatedEventId, storedEvent.getId());
     }
 }
