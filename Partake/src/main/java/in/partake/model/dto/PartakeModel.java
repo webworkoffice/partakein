ebe2e74cfc863e88a13a6c22d8d539ee3ddf5653
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-03-06 13:51:23 +0000

    added some comments.

--- Partake/src/main/java/in/partake/model/dto/PartakeModel.java
+++ Partake/src/main/java/in/partake/model/dto/PartakeModel.java
@@ -1,23 +1,51 @@
 package in.partake.model.dto;
 
+/**
+ * PARTAKE で用いるモデルのベースクラス。
+ * 
+ * キャッシュをのちのち使いたいので、データベースから取得してきたオブジェクトは freeze() される。freeze() されたオブジェクトは変更不能である。
+ * 新たにデータを変更したい場合、copy() を取得してから変更のこと。copy() されたオブジェクトは freeze() が解除される。
+ * 
+ * なお、BinaryData などの大きなオブジェクトでは、unsafe なコピーがなされることがあるので注意すること。
+ * 
+ * @author shinyak
+ *
+ * @param <T>
+ */
 public abstract class PartakeModel<T extends PartakeModel<?>> {
+    /** Model が freeze されていれば true */
     private volatile boolean frozen;
     
     protected PartakeModel() {
         this.frozen = false;
     }
     
+    /**
+     * @return primary key
+     */
     public abstract Object getPrimaryKey();
+    
+    
     public abstract T copy();
     
+    /**
+     * check the object is fronzen. If frozen, UnsupportedOperationExcetpion will be raised.
+     */
     protected void checkFrozen() {
         if (frozen) { throw new UnsupportedOperationException(); }
     }
     
+    /**
+     * @return true if frozen
+     */
     public boolean isFrozen() {
         return frozen;
     }
     
+    /**
+     * freeze the object and return itself.
+     * @return itself.
+     */
     @SuppressWarnings("unchecked")
     public T freeze() {
         this.frozen = true;
