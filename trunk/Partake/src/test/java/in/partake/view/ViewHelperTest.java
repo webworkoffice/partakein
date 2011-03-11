c4f5d7992e61b8b8e9db5ae07ac5918669dfcd86
skypencil <skypencil@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-03-11 12:47:26 +0000

    fixed issue 148.

--- trunk/Partake/src/test/java/in/partake/view/ViewHelperTest.java
+++ trunk/Partake/src/test/java/in/partake/view/ViewHelperTest.java
@@ -4,6 +4,7 @@ import org.apache.struts2.StrutsTestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static in.partake.view.Helper.escapeTwitterResponse;
 import static in.partake.view.Helper.h;
 
 public class ViewHelperTest extends StrutsTestCase {
@@ -30,10 +31,13 @@ public class ViewHelperTest extends StrutsTestCase {
         Assert.assertEquals("\n", h("\n"));
         Assert.assertEquals("\r\n", h("\r\n"));
 
+        Assert.assertEquals("@screen_name", h("@screen_name"));
+        Assert.assertEquals("#hashtag", h("#hashtag"));
         Assert.assertEquals("&amp;&lt;tag&gt;", h("&<tag>"));
         Assert.assertEquals("漢字＆ひらがな", h("漢字＆ひらがな"));
         Assert.assertEquals("サロゲートペア→𠮟", h("サロゲートペア→𠮟"));
         Assert.assertEquals("double &quot;quoted&quot;", h("double \"quoted\""));
+        Assert.assertEquals("&lt;script&gt;&lt;/script&gt;", h("<script></script>"));
     }
 
     
@@ -44,5 +48,38 @@ public class ViewHelperTest extends StrutsTestCase {
         
         System.out.println(sanity);
         Assert.assertTrue(!sanity.contains("script"));
+    }
+
+    @Test
+    public void testEscapeTwitterResponse() {
+        Assert.assertEquals("", escapeTwitterResponse(""));
+        Assert.assertEquals("", escapeTwitterResponse(null));
+
+        Assert.assertEquals(" ", escapeTwitterResponse(" "));
+        Assert.assertEquals("test", escapeTwitterResponse("test"));
+
+        Assert.assertEquals("&amp;", escapeTwitterResponse("&"));
+        Assert.assertEquals("&lt;", escapeTwitterResponse("<"));	// Twitterは返さないはずだけど脆弱性につながると嫌だし念の為にテスト
+        Assert.assertEquals("&gt;", escapeTwitterResponse(">"));	// Twitterは返さないはずだけど脆弱性につながると嫌だし念の為にテスト
+        Assert.assertEquals("&lt;", escapeTwitterResponse("&lt;"));
+        Assert.assertEquals("&gt;", escapeTwitterResponse("&gt;"));
+        Assert.assertEquals("&quot;", escapeTwitterResponse("\""));
+        Assert.assertEquals("&apos;", escapeTwitterResponse("\'"));
+
+        Assert.assertEquals("", escapeTwitterResponse(Character.toString('\0')));    	// NUL
+        Assert.assertEquals("", escapeTwitterResponse(Character.toString('\u202E')));	// RLO
+
+        Assert.assertEquals("\t", escapeTwitterResponse("\t"));
+        Assert.assertEquals("\r", escapeTwitterResponse("\r"));
+        Assert.assertEquals("\n", escapeTwitterResponse("\n"));
+        Assert.assertEquals("\r\n", escapeTwitterResponse("\r\n"));
+
+        Assert.assertEquals("@screen_name", escapeTwitterResponse("@screen_name"));
+        Assert.assertEquals("#hashtag", escapeTwitterResponse("#hashtag"));
+        Assert.assertEquals("&amp;&lt;tag&gt;", escapeTwitterResponse("&<tag>"));
+        Assert.assertEquals("漢字＆ひらがな", escapeTwitterResponse("漢字＆ひらがな"));
+        Assert.assertEquals("サロゲートペア→𠮟", escapeTwitterResponse("サロゲートペア→𠮟"));
+        Assert.assertEquals("double &quot;quoted&quot;", escapeTwitterResponse("double \"quoted\""));
+        Assert.assertEquals("&lt;script&gt;&lt;/script&gt;", escapeTwitterResponse("<script></script>"));
     }
 }
