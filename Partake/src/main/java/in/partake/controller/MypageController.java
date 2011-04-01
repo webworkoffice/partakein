abae01f5e7ae4d2617fa070f6234e0ec4ee5c290
skypencil <skypencil@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-04-01 12:51:43 +0000

    リファクタリングを施しつつ、ロガーを使うように修正。

--- Partake/src/main/java/in/partake/controller/MypageController.java
+++ Partake/src/main/java/in/partake/controller/MypageController.java
@@ -12,6 +12,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionContext;
@@ -21,41 +22,41 @@ import com.opensymphony.xwork2.ActionContext;
  * Servlet implementation class UserMypageController
  */
 public class MypageController extends PartakeActionSupport implements SessionAware {
-	/** */
 	private static final long serialVersionUID = 1L;
-	// private static final Logger logger = Logger.getLogger(MypageController.class);
-	
+	private static final Logger logger = Logger.getLogger(MypageController.class);
+
 	public String show() {
 		User user = getLoginUser();
 		if (user == null) { return LOGIN; }
 
-        try {
-            List<Event> ownedEvents = EventService.get().getEventsOwnedBy(user);
-            List<Event> enrolledEvents = UserService.get().getEnrolledEvents(user.getId());
-            
-            List<Event> enrolled = new ArrayList<Event>();
-            List<Event> finished = new ArrayList<Event>();
-            Date now = new Date();
-            for (Event e : enrolledEvents) {
-            	if (e == null) { continue; }
-            	if (e.getBeginDate().before(now)) {
-            		finished.add(e);
-            	} else {
-            		enrolled.add(e);
-            	}
-            }
-            
-            Collections.sort(enrolled, Event.getComparatorBeginDateAsc());
-            Collections.sort(finished, Event.getComparatorBeginDateAsc());
-            
-            ActionContext.getContext().put(Constants.ATTR_OWNED_EVENTSET, ownedEvents);
-            ActionContext.getContext().put(Constants.ATTR_ENROLLED_EVENTSET, enrolled);
-            ActionContext.getContext().put(Constants.ATTR_FINISHED_EVENTSET, finished);
+		try {
+			List<Event> ownedEvents = EventService.get().getEventsOwnedBy(user);
+			List<Event> enrolledEvents = UserService.get().getEnrolledEvents(user.getId());
 
-            return SUCCESS;
-        } catch (DAOException e) {
-        	e.printStackTrace();
-        	return ERROR;
-        }
+			List<Event> enrolled = new ArrayList<Event>();
+			List<Event> finished = new ArrayList<Event>();
+			Date now = new Date();
+			for (Event e : enrolledEvents) {
+				if (e == null) { continue; }
+				if (e.getBeginDate().before(now)) {
+					finished.add(e);
+				} else {
+					enrolled.add(e);
+				}
+			}
+
+			Collections.sort(enrolled, Event.getComparatorBeginDateAsc());
+			Collections.sort(finished, Event.getComparatorBeginDateAsc());
+
+			ActionContext.getContext().put(Constants.ATTR_OWNED_EVENTSET, ownedEvents);
+			ActionContext.getContext().put(Constants.ATTR_ENROLLED_EVENTSET, enrolled);
+			ActionContext.getContext().put(Constants.ATTR_FINISHED_EVENTSET, finished);
+
+			return SUCCESS;
+		} catch (DAOException e) {
+			logger.warn("データベースエラーです。", e);
+			addActionError("データベースエラーです。");
+			return ERROR;
+		}
 	}
 }
