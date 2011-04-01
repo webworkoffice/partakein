4e764dbe6b6f2b660cebdd6c7b49d26db22202f3
skypencil <skypencil@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-04-01 12:50:40 +0000

    存在しないimageIdを指定した場合にNOT_FOUNDを返すように修正。

---
+++ Partake/src/test/java/in/partake/controller/EventsImageControllerTest.java
@@ -0,0 +1,27 @@
+package in.partake.controller;
+
+import org.apache.struts2.StrutsTestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.opensymphony.xwork2.ActionProxy;
+
+public class EventsImageControllerTest extends StrutsTestCase {
+	@Test
+	public void testToShowWithoutImageId() {
+		request.removeParameter(EventsImageController.IMAGE_ID_PARAM_NAME);
+
+		ActionProxy proxy = getActionProxy("/events/images/");
+		EventsImageController controller = (EventsImageController) proxy.getAction();
+		Assert.assertEquals(PartakeActionSupport.NOT_FOUND, controller.show());
+	}
+
+	@Test
+	public void testToGetNonexistentImage() {
+		request.setParameter(EventsImageController.IMAGE_ID_PARAM_NAME, "NonexistentImageId");
+
+		ActionProxy proxy = getActionProxy("/events/images/");
+		EventsImageController controller = (EventsImageController) proxy.getAction();
+		Assert.assertEquals(PartakeActionSupport.NOT_FOUND, controller.show());
+	}
+}
