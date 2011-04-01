4186cf2bedeefa0bd7a16d8ac8d3721ac3683e82
skypencil <skypencil@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-04-01 12:51:43 +0000

    リファクタリングを施しつつ、ロガーを使うように修正。

--- trunk/Partake/src/main/java/in/partake/controller/UsersController.java
+++ trunk/Partake/src/main/java/in/partake/controller/UsersController.java
@@ -24,98 +24,90 @@ import com.opensymphony.xwork2.ActionContext;
 
 
 public class UsersController extends PartakeActionSupport {
-	/** */
 	private static final long serialVersionUID = 1L;
 	private static final Logger logger = Logger.getLogger(UsersController.class);
-	
+
 	// ----------------------------------------------------------------------
-    
-    public String show() {
-    	// TODO MypageController#show() のコードをほぼ流用できる
-    	try {
-	    	String userId = getParameter("userId");
-	    	if (StringUtils.isEmpty(userId)) { return ERROR; }
-	    	    	
-	    	UserEx user = UserService.get().getUserExById(userId);
-	    	if (user == null) { return NOT_FOUND; }
-	    	
-	    	UserPreference pref = UserService.get().getUserPreference(userId);
-	    	if (pref == null) {
-	    	    logger.error("UserPreference is null. Something wrong.");
-	    	    return NOT_FOUND;
-	    	}
-	    	
-	    	if (pref.isProfilePublic()) {
-		    	attributes.put(Constants.ATTR_SHOWING_USER, user);	
-		    	
-		    	try {
-		            List<Event> owned = EventService.get().getEventsOwnedBy(user); 
-		            List<Event> enrolledEvents = UserService.get().getEnrolledEvents(user.getId());
-		            
-		            List<Event> enrolled = new ArrayList<Event>();
-		            List<Event> finished = new ArrayList<Event>();
-		            Date now = new Date();
-		            
-		            for (Event e : enrolledEvents) {
-		            	if (e == null) { continue; }
-		            	if (e.getBeginDate().before(now)) {
-		            		finished.add(e);
-		            	} else {
-		            		enrolled.add(e);
-		            	}
-		            }
-		            // このページは他者に見せるものなので、見せていいものだけを返す
-		            IsPublicPredicate predicate = new IsPublicPredicate();
-		            CollectionUtils.filter(owned,predicate);
-		            CollectionUtils.filter(enrolled, predicate);
-		            CollectionUtils.filter(finished, predicate);
 
-		            Collections.sort(enrolled, Event.getComparatorBeginDateAsc());
-		            Collections.sort(finished, Event.getComparatorBeginDateAsc());
-		            
-		            ActionContext.getContext().put(Constants.ATTR_OWNED_EVENTSET, owned);
-		            ActionContext.getContext().put(Constants.ATTR_ENROLLED_EVENTSET, enrolled);
-		            ActionContext.getContext().put(Constants.ATTR_FINISHED_EVENTSET, finished);
+	public String show() {
+		// TODO MypageController#show() のコードをほぼ流用できる
+		try {
+			String userId = getParameter("userId");
+			if (StringUtils.isEmpty(userId)) { return ERROR; }
 
-		            return SUCCESS;
-		        } catch (DAOException e) {
-		        	e.printStackTrace();
-		        	return ERROR;
-		        }
-		        
-	    	} else {
-	    		return PROHIBITED;
-	    	}
-	    	
-    	} catch (DAOException e) {
-    		e.printStackTrace();
-    		addActionError("データベースエラーです。");
-    		return ERROR;
-    	}
-    }
+			UserEx user = UserService.get().getUserExById(userId);
+			if (user == null) { return NOT_FOUND; }
 
-    /**
-     * revoke calendar id. A new calendar id is automatically generated.
-     * @return
-     */
-    public String revokeCalendar() {
-    	try {
-    		UserEx user = getLoginUser();
-    		if (user == null) { return ERROR; }
-    		
-    		CalendarService.get().revokeCalendar(user);
-    		
-    		// TODO: Unfortunately, the [user] must be updated to reflect this calendar revocation.
-    		// For convenient way, we retrieve user again, and set it to the session.    		
-            user = UserService.get().getUserExById(user.getId());
-            session.put(Constants.ATTR_USER, user);
-    		
-    		return SUCCESS;
-    	} catch (DAOException e) {
-    	    logger.warn("revokeCalendar() failed.", e);
-    		return ERROR;
-    	}
-    }
+			UserPreference pref = UserService.get().getUserPreference(userId);
+			if (pref == null) {
+				logger.error("UserPreference is null. Something wrong.");
+				return NOT_FOUND;
+			}
+
+			if (pref.isProfilePublic()) {
+				attributes.put(Constants.ATTR_SHOWING_USER, user);
+				List<Event> owned = EventService.get().getEventsOwnedBy(user); 
+				List<Event> enrolledEvents = UserService.get().getEnrolledEvents(user.getId());
+
+				List<Event> enrolled = new ArrayList<Event>();
+				List<Event> finished = new ArrayList<Event>();
+				Date now = new Date();
+
+				for (Event e : enrolledEvents) {
+					if (e == null) { continue; }
+					if (e.getBeginDate().before(now)) {
+						finished.add(e);
+					} else {
+						enrolled.add(e);
+					}
+				}
+
+				// このページは他者に見せるものなので、見せていいイベントだけを返す
+				IsPublicPredicate predicate = new IsPublicPredicate();
+				CollectionUtils.filter(owned, predicate);
+				CollectionUtils.filter(enrolled, predicate);
+				CollectionUtils.filter(finished, predicate);
+
+				Collections.sort(enrolled, Event.getComparatorBeginDateAsc());
+				Collections.sort(finished, Event.getComparatorBeginDateAsc());
+
+				ActionContext.getContext().put(Constants.ATTR_OWNED_EVENTSET, owned);
+				ActionContext.getContext().put(Constants.ATTR_ENROLLED_EVENTSET, enrolled);
+				ActionContext.getContext().put(Constants.ATTR_FINISHED_EVENTSET, finished);
+
+				return SUCCESS;
+			} else {
+				return PROHIBITED;
+			}
+		} catch (DAOException e) {
+			logger.warn("データベースエラーです。", e);
+			addActionError("データベースエラーです。");
+			return ERROR;
+		}
+	}
+
+	/**
+	 * revoke calendar id. A new calendar id is automatically generated.
+	 * @return
+	 */
+	public String revokeCalendar() {
+		try {
+			UserEx user = getLoginUser();
+			if (user == null) { return ERROR; }
+
+			CalendarService.get().revokeCalendar(user);
+
+			// TODO: Unfortunately, the [user] must be updated to reflect this calendar revocation.
+			// For convenient way, we retrieve user again, and set it to the session.    		
+			user = UserService.get().getUserExById(user.getId());
+			session.put(Constants.ATTR_USER, user);
+
+			return SUCCESS;
+		} catch (DAOException e) {
+			logger.warn("revokeCalendar() failed.", e);
+			return ERROR;
+		}
+	}
 
 	private static class IsPublicPredicate implements Predicate {
 		@Override
