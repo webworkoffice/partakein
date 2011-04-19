20c3aa9a76de487759f8a7ea8499c5a77296c8ed
skypencil <skypencil@gmail.com>
2011-04-19 02:28:39 +0000

    IMessageAccess doesn't tell about order of the DataIterator's value. This problem will raise some problems like Issue 142.

--- Partake/src/main/java/in/partake/model/dao/IMessageAccess.java
+++ Partake/src/main/java/in/partake/model/dao/IMessageAccess.java
@@ -3,6 +3,7 @@ package in.partake.model.dao;
 import in.partake.model.dto.Message;
 
 public interface IMessageAccess extends IAccess<Message, String> {
-    public String getFreshId(PartakeConnection con) throws DAOException;    
+    public String getFreshId(PartakeConnection con) throws DAOException;
+    // FIXME このメソッドが返すDataIteratorの要素が、何順に並んでいるのかが不明。MessageServiceは新しい順に並んでいることを期待している。
     public DataIterator<Message> findByEventId(PartakeConnection con, String eventId) throws DAOException;
 }
