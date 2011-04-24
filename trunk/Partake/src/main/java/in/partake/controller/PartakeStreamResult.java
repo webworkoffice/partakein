ef21c695ebf196035228c1c230848ec61dcec851
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-04-24 21:43:25 +0000

    added PartakeStreamResult for better http handling.

---
+++ trunk/Partake/src/main/java/in/partake/controller/PartakeStreamResult.java
@@ -0,0 +1,69 @@
+package in.partake.controller;
+
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.struts2.dispatcher.HttpHeaderResult;
+import org.apache.struts2.dispatcher.StreamResult;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.util.ValueStack;
+
+public class PartakeStreamResult extends StreamResult {
+    private static final long serialVersionUID = 1L;
+    private HttpHeaderResult headerResult;
+    private Map<String, String> additionalHeaders;
+    
+    public PartakeStreamResult() {
+        this.headerResult = new HttpHeaderResult();
+    }
+
+    public PartakeStreamResult(InputStream in) {
+        super(in);
+        this.headerResult = new HttpHeaderResult();
+    }
+
+    public void execute(ActionInvocation invocation) throws Exception {
+        resolveParameters(invocation);
+        
+        if (additionalHeaders != null) {
+            for (Entry<String, String> entry : additionalHeaders.entrySet()) {
+                headerResult.addHeader(entry.getKey(), entry.getValue());
+            }
+        }
+        
+        headerResult.execute(invocation);
+        super.execute(invocation);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resolveParameters(ActionInvocation invocation) {
+        ValueStack stack = ActionContext.getContext().getValueStack(); 
+        
+        String status = stack.findString("status");
+        if (status != null) {
+            setStatus(Integer.parseInt(status));
+        }
+
+        Object headers = stack.findValue("headers");
+        if (headers instanceof Map) {
+            setAdditionalHeaders((Map<String, String>) headers);
+        }
+    }
+    
+    // ----------------------------------------------------------------------
+    
+    public void setStatus(int status) {
+        if (status >= 400) {
+            headerResult.setError(status);
+        } else {
+            headerResult.setStatus(status);
+        }
+    }
+    
+    public void setAdditionalHeaders(Map<String, String> additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
+}
