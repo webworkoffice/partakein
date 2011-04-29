762ec3bdde62293444e916b164ca02732cb4a68e
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-04-29 07:55:01 +0000

    added better error handling for API.

--- Partake/src/main/java/in/partake/controller/interceptor/PartakeExceptionInterceptor.java
+++ Partake/src/main/java/in/partake/controller/interceptor/PartakeExceptionInterceptor.java
@@ -4,6 +4,9 @@ import in.partake.controller.PartakeInvalidResultException;
 import in.partake.controller.PartakeResultException;
 import in.partake.model.dao.DAOException;
 import in.partake.resource.Constants;
+import in.partake.resource.I18n;
+
+import org.apache.log4j.Logger;
 
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionInvocation;
@@ -12,12 +15,18 @@ import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 public class PartakeExceptionInterceptor extends AbstractInterceptor {
     /** */
     private static final long serialVersionUID = 1L;
+    private static final Logger logger = Logger.getLogger(PartakeExceptionInterceptor.class);
+
 
     @Override
     public String intercept(ActionInvocation invocation) throws Exception {
         try {
             return invocation.invoke();
+        } catch (RuntimeException e) {
+            logger.error("Uncaught Runtime Exception", e);
+            return "error";
         } catch (DAOException e) {
+            logger.error(I18n.t(I18n.DATABASE_ERROR), e);
             return "error";
         } catch (PartakeInvalidResultException e) {
             // invalid は redirect がはいるので、session に保持しておく
