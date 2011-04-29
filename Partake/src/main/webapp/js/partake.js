0f900e3a8a75785ca2709ff58dd1cd3ab26269f1
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-04-29 08:46:47 +0000

    Adding partake.js, a PARTAKE basic API library. This is under implementation.

---
+++ Partake/src/main/webapp/js/partake.js
@@ -0,0 +1,25 @@
+// partake の基本 API を便利に使うための javascript library です。jQuery に依存します。
+// $partake のみが公開されます。
+
+// *** UNDER IMPLEMENTATION ***
+
+function $partake() {
+};
+
+/**
+ * @param {!String} userId
+ * @param {!String} eventId
+ * @param {!String} status
+ * 
+ * for example
+ * $partke.changeAttendance(...).success(function(json) {...} ).error(function(json) {...});
+ */
+$partke.changeAttendance = function(userId, eventId, status) {
+	var arg = {
+		userId: userId,
+		eventId: eventId,
+		status: status
+	};
+	
+	return $.post('/api/attendance/change', arg);
+}; 
