42f4a018b1e2393c640febdb22b94f1eaee77776
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-04-29 07:57:25 +0000

    updated AttendanceAction to pass tests.

--- Partake/src/main/java/in/partake/model/dto/auxiliary/AttendanceStatus.java
+++ Partake/src/main/java/in/partake/model/dto/auxiliary/AttendanceStatus.java
@@ -12,12 +12,21 @@ public enum AttendanceStatus {
         if ("".equals(str)) { return SAFE_VALUE; }
         
         if ("present".equalsIgnoreCase(str)) { return PRESENT; }
-        if ("absent".equalsIgnoreCase(str)) { return ABSENT; }
+        if ("absent".equalsIgnoreCase(str))  { return ABSENT; }
+        if ("unknown".equalsIgnoreCase(str)) { return UNKNOWN; }
         
         try {
             return valueOf(str);
         } catch (IllegalArgumentException e) {
             return SAFE_VALUE;
         }
+    }
+    
+    public static boolean isValueOf(String str) {
+        if ("present".equalsIgnoreCase(str)) { return true; }
+        if ("absent".equalsIgnoreCase(str))  { return true; }
+        if ("unknown".equalsIgnoreCase(str)) { return true; }
+
+        return false;
     }
 }
