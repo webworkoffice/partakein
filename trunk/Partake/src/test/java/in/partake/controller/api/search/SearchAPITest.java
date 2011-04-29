0640cc431e9290de3e4825a318cfe0193416f11f
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-04-29 07:57:49 +0000

    update SearchAPI (but not pass the tests yet).

--- trunk/Partake/src/test/java/in/partake/controller/api/search/SearchAPITest.java
+++ trunk/Partake/src/test/java/in/partake/controller/api/search/SearchAPITest.java
@@ -1,9 +1,9 @@
 package in.partake.controller.api.search;
 
 import in.partake.controller.api.APIControllerTest;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
-import org.apache.commons.lang.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -16,14 +16,12 @@ public class SearchAPITest extends APIControllerTest {
         ActionProxy proxy = getActionProxy("/api/search");
 
         proxy.execute();
-
+        this.assertResultOK(proxy);
+        
         JSONObject obj = getJSON(proxy);
-        
-        Assert.assertEquals(200, response.getStatus());
-        Assert.assertEquals("ok", obj.get("result"));
-        
-        // 任意のオーダーで Event が出力される。
-        throw new RuntimeException("Not implemented yet");
+        JSONArray events = obj.getJSONArray("events");
+        Assert.assertNotNull(events);
+        Assert.assertTrue(events.size() > 0);
     }
         
     @Test
@@ -32,12 +30,7 @@ public class SearchAPITest extends APIControllerTest {
         addParameter(proxy, "maxNum", "-1");
 
         proxy.execute();
-
-        JSONObject obj = getJSON(proxy);
-        
-        Assert.assertEquals(400, response.getStatus());
-        Assert.assertEquals("invalid", obj.get("result"));
-        Assert.assertFalse(StringUtils.isBlank((String) obj.get("reason")));
+        assertResultInvalid(proxy);
     }
     
     @Test
@@ -46,12 +39,7 @@ public class SearchAPITest extends APIControllerTest {
         addParameter(proxy, "maxNum", "");  // should not be empty
 
         proxy.execute();
-
-        JSONObject obj = getJSON(proxy);
-        
-        Assert.assertEquals(400, response.getStatus());
-        Assert.assertEquals("invalid", obj.get("result"));
-        Assert.assertFalse(StringUtils.isBlank((String) obj.get("reason")));
+        assertResultInvalid(proxy);
     }
     
     @Test
@@ -60,12 +48,7 @@ public class SearchAPITest extends APIControllerTest {
         addParameter(proxy, "maxNum", String.valueOf(0x100000001L));  // exceeds 32bit integer 0xFFFFFFF
 
         proxy.execute();
-
-        JSONObject obj = getJSON(proxy);
-        
-        Assert.assertEquals(400, response.getStatus());
-        Assert.assertEquals("invalid", obj.get("result"));
-        Assert.assertFalse(StringUtils.isBlank((String) obj.get("reason")));
+        assertResultInvalid(proxy);
     }
 
     @Test
@@ -74,12 +57,15 @@ public class SearchAPITest extends APIControllerTest {
         addParameter(proxy, "maxNum", SearchAction.MAX_NUM + 1);
 
         proxy.execute();
-
-        JSONObject obj = getJSON(proxy);
-        
-        Assert.assertEquals(400, response.getStatus());
-        Assert.assertEquals("invalid", obj.get("result"));
-        Assert.assertFalse(StringUtils.isBlank((String) obj.get("reason")));
+        assertResultInvalid(proxy);
     }
 
+    @Test
+    public void testToSearchWithInvalidArg1() throws Exception {
+        ActionProxy proxy = getActionProxy("/api/search");
+        addParameter(proxy, "query", "ほげほげ\"");
+        
+        proxy.execute();
+        assertResultInvalid(proxy);
+    }
 }
