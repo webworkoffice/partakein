370ce1f9318f5bfe7a758ac6ce59c39e7210e3c7
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-05-10 09:41:06 +0000

    added event-get API.

--- Partake/src/main/java/in/partake/controller/api/event/EventAction.java
+++ Partake/src/main/java/in/partake/controller/api/event/EventAction.java
@@ -1,51 +1,46 @@
 package in.partake.controller.api.event;
 
+import net.sf.json.JSONObject;
+
+import org.apache.commons.lang.StringUtils;
+
 import in.partake.controller.api.PartakeAPIActionSupport;
+import in.partake.model.EventEx;
+import in.partake.model.UserEx;
+import in.partake.model.dao.DAOException;
+import in.partake.model.dto.auxiliary.UserPermission;
+import in.partake.service.EventService;
 
 public class EventAction extends PartakeAPIActionSupport {
     private static final long serialVersionUID = 1L;
 
-    public String get() {
-        throw new RuntimeException("Not implemented yet");
-    }
-// public String getEvent() {
-//     String eventId = getParameter("eventId");
-//     if (StringUtils.isEmpty(eventId)) { return INVALID; }
-//     
-//     UserEx loginUser = getLoginUser();
-//     
-//     try {
-//         EventEx event = EventService.get().getEventExById(eventId);
-//         if (StringUtils.isEmpty(eventId)) { return NOT_FOUND; }
-//         
-//         if (event.isPrivate()) {
-//             // TODO: EventsController とコードが同じなので共通化するべき　
-//             
-//             // owner および manager は見ることが出来る。
-//             String passcode = (String)session.get("event:" + eventId);
-//             if (passcode == null) { passcode = getParameter("passcode"); }
-//             
-//             if (loginUser != null && event.hasPermission(loginUser, UserPermission.EVENT_PRIVATE_EVENT)) {
-//                 // OK. You have the right to show this event.
-//             } else if (StringUtils.equals(event.getPasscode(), passcode)) {
-//                 // OK. The same passcode. 
-//             } else {
-//                 // public でなければ、passcode を入れなければ見ることが出来ない
-//                 return PROHIBITED;
-//             }
-//         }
-//         
-//         String json = event.toJSON();
-//         inputStream = new ByteArrayInputStream(json.getBytes("utf-8"));
-//         return SUCCESS;
-//         
-//     } catch (DAOException e) {
-//         logger.error(I18n.t(I18n.DATABASE_ERROR), e);
-//         return redirectDBError();
-//     } catch (UnsupportedEncodingException e) {
-//         logger.error("UnsupportedEncodingException", e);
-//         return redirectError("文字列のエンコード指定が誤っています。");
-//     }
-// }
+    public String get() throws DAOException {
+        String eventId = getParameter("eventId");
+        if (StringUtils.isBlank(eventId)) { return renderInvalid("invalid eventId was specified."); }
+        
+        EventEx event = EventService.get().getEventExById(eventId);
+        if (event == null) { return renderInvalid("invalid eventId was specified."); }
+            
+        if (event.isPrivate()) {
+            // TODO: EventsController とコードが同じなので共通化するべき　
+      
+            // owner および manager は見ることが出来る。
+            String passcode = (String) session.get("event:" + eventId);
+            if (passcode == null) { passcode = getParameter("passcode"); }
+      
+            UserEx loginUser = getLoginUser();
+            if (loginUser != null && event.hasPermission(loginUser, UserPermission.EVENT_PRIVATE_EVENT)) {
+                // OK. You have the right to show this event.
+            } else if (StringUtils.equals(event.getPasscode(), passcode)) {
+                // OK. The same passcode. 
+            } else {
+                // public でなければ、passcode を入れなければ見ることが出来ない
+                return renderForbidden();
+            }
+        }
 
+        JSONObject obj = new JSONObject();
+        obj.put("event", event.toJSON());
+        return renderOK(obj);
+    }
 }
