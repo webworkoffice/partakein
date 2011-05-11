cceb1f36ffbe624487fafa7aa41bec6dd11650c8
skypencil <skypencil@gmail.com>
2011-05-11 08:03:15 +0000

    we should use Logger#error(String, Throwable) instead of Logger#(String) if we can.

--- Partake/src/main/java/in/partake/controller/EventParticipantsController.java
+++ Partake/src/main/java/in/partake/controller/EventParticipantsController.java
@@ -26,135 +26,135 @@ import au.com.bytecode.opencsv.CSVWriter;
 
 public class EventParticipantsController extends PartakeActionSupport {
     /** */
-    private static final long serialVersionUID = 1L;    
+    private static final long serialVersionUID = 1L;
     private static final Logger logger = Logger.getLogger(EventParticipantsController.class);
-    
+
     private String contentType = null;
     private ByteArrayInputStream inputStream = null;
     private String eventId;
-    
+
     public String getContentType() {
         return this.contentType;
     }
-    
+
     public ByteArrayInputStream getInputStream() {
         return this.inputStream;
     }
-    
+
     public String getEventId() {
         return eventId;
     }
-    
+
     // ----------------------------------------------------------------------
-    
+
     public String showParticipants() throws PartakeResultException {
         UserEx user = ensureLogin();
-        
+
         eventId = getParameter("eventId");
         if (eventId == null) { throw new PartakeResultException(ERROR); }
-        
+
         try {
-            EventEx event = EventService.get().getEventExById(eventId); 
+            EventEx event = EventService.get().getEventExById(eventId);
             if (event == null) { throw new PartakeResultException(ERROR); }
-    
+
             // Only owner can retrieve the participants list.
             if (!event.hasPermission(user, UserPermission.EVENT_PARTICIPATION_LIST)) {
                 addErrorMessage("イベント参加者の取得権限がありません。");
                 throw new PartakeResultException(PROHIBITED);
             }
-            
+
             List<EnrollmentEx> participations = EventService.get().getEnrollmentEx(eventId);
             ParticipationList list = event.calculateParticipationList(participations);
-            
+
             attributes.put(Constants.ATTR_EVENT, event);
             attributes.put(Constants.ATTR_PARTICIPATIONLIST, list);
-            
+
             return SUCCESS;
         } catch (DAOException e) {
-            logger.error(I18n.t(I18n.DATABASE_ERROR));
+            logger.error(I18n.t(I18n.DATABASE_ERROR), e);
             throw new PartakeResultException(ERROR);
         }
     }
 
     public String makeAttendantVIP() throws PartakeResultException {
         UserEx user = ensureLogin();
-        
+
         eventId = getParameter("eventId");
         if (StringUtils.isEmpty(eventId)) { throw new PartakeResultException(ERROR); }
-        
+
         String userId = getParameter("userId");
         if (StringUtils.isEmpty(userId)) { throw new PartakeResultException(ERROR); }
-        
+
         boolean vip = "true".equals(getParameter("vip"));
-        
+
         try {
-            EventEx event = EventService.get().getEventExById(eventId); 
+            EventEx event = EventService.get().getEventExById(eventId);
             if (event == null) { throw new PartakeResultException(ERROR); }
-    
+
             // Only owner can retrieve the participants list.
             if (!event.hasPermission(user, UserPermission.EVENT_EDIT_PARTICIPANTS)) {
                 addErrorMessage("イベント参加者の編集権限がありません。");
                 throw new PartakeResultException(PROHIBITED);
             }
-            
+
             if (EventService.get().makeAttendantVIP(eventId, userId, vip)) {
                 return SUCCESS;
             } else {
                 return ERROR;
             }
-            
+
         } catch (DAOException e) {
-            logger.error(I18n.t(I18n.DATABASE_ERROR));
-            throw new PartakeResultException(ERROR);            
+            logger.error(I18n.t(I18n.DATABASE_ERROR), e);
+            throw new PartakeResultException(ERROR);
         }
     }
-    
+
     public String removeAttendant() throws PartakeResultException {
         UserEx user = ensureLogin();
-        
+
         eventId = getParameter("eventId");
         if (StringUtils.isEmpty(eventId)) { throw new PartakeResultException(ERROR); }
-        
+
         String userId = getParameter("userId");
         if (StringUtils.isEmpty(userId)) { throw new PartakeResultException(ERROR); }
-        
+
         try {
-            EventEx event = EventService.get().getEventExById(eventId); 
+            EventEx event = EventService.get().getEventExById(eventId);
             if (event == null) { throw new PartakeResultException(ERROR); }
-    
+
             // Only owner can retrieve the participants list.
             if (!event.hasPermission(user, UserPermission.EVENT_EDIT_PARTICIPANTS)) {
                 addErrorMessage("イベント参加者の編集権限がありません。");
                 throw new PartakeResultException(PROHIBITED);
             }
-            
+
             if (EventService.get().removeEnrollment(eventId, userId)) {
                 return SUCCESS;
             } else {
                 return ERROR;
             }
-            
+
         } catch (DAOException e) {
-            logger.error(I18n.t(I18n.DATABASE_ERROR));
-            throw new PartakeResultException(ERROR);            
-        }        
+            logger.error(I18n.t(I18n.DATABASE_ERROR), e);
+            throw new PartakeResultException(ERROR);
+        }
     }
-    
+
     /**
-     * 
+     *
      * @return
      */
     public String showCSV() throws PartakeResultException {
         try {
-            ParticipationList list = calculateParticipationList(); 
+            ParticipationList list = calculateParticipationList();
 
-            
+
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             CSVWriter writer = new CSVWriter(new OutputStreamWriter(baos));
-            
+
             for (Enrollment participation : list.getEnrolledParticipations()) {
                 UserEx user = UserService.get().getUserExById(participation.getUserId());
-                
+
                 String[] lst = new String[4];
                 lst[0] = user.getTwitterLinkage().getScreenName();
                 // TODO why don't you use in.partake.view.util.Helper.enrollmentStatus to get status?
@@ -167,13 +167,13 @@ public class EventParticipantsController extends PartakeActionSupport {
                     logger.warn("SHOULD NOT HAPPEN");
                 }
                 lst[2] = participation.getComment();
-                lst[3] = participation.getModifiedAt().toString();                
+                lst[3] = participation.getModifiedAt().toString();
                 writer.writeNext(lst);
             }
-            
+
             for (Enrollment participation : list.getSpareParticipations()) {
                 UserEx user = UserService.get().getUserExById(participation.getUserId());
-                
+
                 String[] lst = new String[4];
                 lst[0] = user.getTwitterLinkage().getScreenName();
                 if (ParticipationStatus.ENROLLED.equals(participation.getStatus())) {
@@ -185,7 +185,7 @@ public class EventParticipantsController extends PartakeActionSupport {
                     logger.warn("SHOULD NOT HAPPEN");
                 }
                 lst[2] = participation.getComment();
-                lst[3] = participation.getModifiedAt().toString();                
+                lst[3] = participation.getModifiedAt().toString();
                 writer.writeNext(lst);
             }
 
@@ -197,40 +197,40 @@ public class EventParticipantsController extends PartakeActionSupport {
             return SUCCESS;
         } catch (IOException e) {
             e.printStackTrace();
-            return ERROR;            
+            return ERROR;
         } catch (DAOException e) {
             e.printStackTrace();
             return ERROR;
         }
     }
-    
+
     /**
      * eventId から participation list を計算する。
-     * 
+     *
      * @param user
      * @return
      * @throws PartakeResultException
      */
     private ParticipationList calculateParticipationList() throws PartakeResultException {
         UserEx user = ensureLogin();
-        
+
         eventId = getParameter("eventId");
         if (eventId == null) { throw new PartakeResultException(ERROR); }
-        
+
         try {
-            EventEx event = EventService.get().getEventExById(eventId); 
+            EventEx event = EventService.get().getEventExById(eventId);
             if (event == null) { throw new PartakeResultException(ERROR); }
-    
+
             // Only owner can retrieve the participants list.
             if (!event.hasPermission(user, UserPermission.EVENT_PARTICIPATION_LIST)) {
                 addErrorMessage("イベント参加者の取得権限がありません。");
                 throw new PartakeResultException(PROHIBITED);
             }
-            
+
             List<EnrollmentEx> participations = EventService.get().getEnrollmentEx(eventId);
             return event.calculateParticipationList(participations);
         } catch (DAOException e) {
-            logger.error(I18n.t(I18n.DATABASE_ERROR));
+            logger.error(I18n.t(I18n.DATABASE_ERROR), e);
             throw new PartakeResultException(ERROR);
         }
     }
