91e264aa781417c487452d075d7ce2b414a505fe
skypencil@gmail.com <skypencil@gmail.com@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-05-11 08:03:15 +0000

    we should use Logger#error(String, Throwable) instead of Logger#(String) if we can.

--- trunk/Partake/src/main/java/in/partake/controller/MypageController.java
+++ trunk/Partake/src/main/java/in/partake/controller/MypageController.java
@@ -39,8 +39,8 @@ public class MypageController extends PartakeActionSupport implements SessionAwa
                 managing.addAll(EventService.get().getEventsManagedBy(user));
 
                 // TODO: 自分自身が manager に含まれていたら２つでるんじゃない？
-            }            
-            
+            }
+
             List<Event> enrolledEvents = UserService.get().getEnrolledEvents(user.getId());
             List<Event> enrolled = new ArrayList<Event>();
             List<Event> finished = new ArrayList<Event>();
@@ -53,17 +53,17 @@ public class MypageController extends PartakeActionSupport implements SessionAwa
             		enrolled.add(e);
             	}
             }
-            
+
             Collections.sort(enrolled, Event.getComparatorBeginDateAsc());
             Collections.sort(finished, Event.getComparatorBeginDateAsc());
-            
+
             ActionContext.getContext().put(Constants.ATTR_OWNED_EVENTSET, managing); // TODO: OWNED になってるけど managing である。
             ActionContext.getContext().put(Constants.ATTR_ENROLLED_EVENTSET, enrolled);
             ActionContext.getContext().put(Constants.ATTR_FINISHED_EVENTSET, finished);
 
             return SUCCESS;
         } catch (DAOException e) {
-            logger.error(I18n.t(I18n.DATABASE_ERROR));
+            logger.error(I18n.t(I18n.DATABASE_ERROR), e);
             return redirectDBError();
         }
 	}
--- trunk/Partake/src/main/java/in/partake/controller/ToppageController.java
+++ trunk/Partake/src/main/java/in/partake/controller/ToppageController.java
@@ -18,7 +18,7 @@ public class ToppageController extends PartakeActionSupport {
 	private static final long serialVersionUID = 1L;
 	private static final Logger logger = Logger.getLogger(ToppageController.class);
 	private static final int DISPLAYED_EVENTS = 5;
-	
+
 	public String index() {
 		// 最近登録されたイベントを出す。
 		// TODO: 毎回表示するの大変なので、cache するべきですよねー。なんとかならないかな。
@@ -29,21 +29,21 @@ public class ToppageController extends PartakeActionSupport {
 			logger.warn("Loading recent events failed.", e);
 			attributes.put(Constants.ATTR_RECENT_EVENTS, Collections.EMPTY_LIST);
 		}
-		
+
 		// もしログインしていれば、最近のイベントを表示する。
 		UserEx user = getLoginUser();
 		if (user != null) {
 		    try {
-		        attributes.put(Constants.ATTR_OWNED_EVENTSET, EventService.get().getUnfinishedEventsOwnedBy(user.getId())); 
-		        attributes.put(Constants.ATTR_ENROLLED_EVENTSET, EventService.get().getUnfinishedEnrolledEvents(user.getId())); 
+		        attributes.put(Constants.ATTR_OWNED_EVENTSET, EventService.get().getUnfinishedEventsOwnedBy(user.getId()));
+		        attributes.put(Constants.ATTR_ENROLLED_EVENTSET, EventService.get().getUnfinishedEnrolledEvents(user.getId()));
 		    } catch (DAOException e) {
-		        logger.error(I18n.t(I18n.DATABASE_ERROR));
+		        logger.error(I18n.t(I18n.DATABASE_ERROR), e);
 		    }
 		}
-		
+
 		return SUCCESS;
 	}
-	
+
 	public String alwaysSuccess() {
 	    return SUCCESS;
 	}
