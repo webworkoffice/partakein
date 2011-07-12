e7ce8800739b3b95702a5b85eac68abbcf934524
kawanaka.shinya <kawanaka.shinya@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-07-12 01:10:35 +0000

    added AccountAPI interface, but it is not implemented yet.

---
+++ trunk/Partake/src/test/java/in/partake/controller/api/account/AccountAPITest.java
@@ -0,0 +1,18 @@
+package in.partake.controller.api.account;
+
+import in.partake.controller.api.APIControllerTest;
+
+import org.junit.Test;
+
+public class AccountAPITest extends APIControllerTest {
+
+    @Test
+    public void testToGetWithLogin() throws Exception {
+        throw new RuntimeException("Not implemented yet");
+    }
+    
+    @Test
+    public void tstToGetWithoutLogin() throws Exception {
+        throw new RuntimeException("Not implemented yet");
+    }
+}
