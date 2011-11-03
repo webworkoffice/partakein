994755a44407210dc4e7ae17a37ced5f1d9afce3
skypencil <skypencil@35327b65-f2d5-03ad-2307-039ebe4172a5>
2011-11-03 09:39:31 +0000

    automated removing backup. see: https://groups.google.com/group/partake-dev/browse_thread/thread/891663bb6ccc5cc2

---
+++ trunk/PartakeServer/remove_old_backups.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+PG_BACKUP_DIR=$1
+TODAY=`date +%s -d $2`
+REAL_TODAY=`date +%s`
+OFFSET=`echo "($REAL_TODAY-$TODAY)/60/60/24" | bc`
+
+one_of() {
+  TARGET=$1
+  shift
+  ENTRY=$1
+  shift
+  while [ $? -eq 0 ]
+  do
+    if [ $ENTRY -eq $TARGET ]
+    then
+      return 0
+    fi
+    ENTRY=$1
+    shift
+  done
+  return 1
+}
+
+remove(){
+  DAY_COUNT=`echo $1+$OFFSET | bc`
+  shift
+
+  for FILE in `find "$PG_BACKUP_DIR" -type f -mtime +$DAY_COUNT`
+  do
+    DAY=`ls -l "$FILE" | awk '{print $6}' | awk 'BEGIN{FS="-"}{print $3}'`
+    one_of $DAY $@
+    if [ $? -eq 0 ]
+    then
+      : # its day of month is specified, so keep this file as weekly backup
+    else
+      rm "$FILE"
+    fi
+  done
+}
+
+# remove dumps which is than 1 year old 
+remove 365
+
+# remove dumps which is than 1 month old, exclude monthly backup
+remove 30  1
+
+# remove dumps which is than 7 days old, exclude weekly backup
+remove 7   1 8 15 22 29
+
