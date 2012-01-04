5edf341e7368d50c18dd25c9f6499c89c263d5ab
mayah@mayah.jp <mayah@mayah.jp@35327b65-f2d5-03ad-2307-039ebe4172a5>
2012-01-04 05:13:59 +0000

    Merged Cassandra/JPA tests. They can be changed using unittest.partake.properties file. Fix Issue 215.

---
+++ trunk/Partake/src/test/java/in/partake/model/dao/CalendarLinkageAccessTest.java
@@ -0,0 +1,16 @@
+package in.partake.model.dao;
+
+import in.partake.model.dto.CalendarLinkage;
+import org.junit.Before;
+
+public class CalendarLinkageAccessTest extends AbstractDaoTestCaseBase<ICalendarLinkageAccess, CalendarLinkage, String> {
+    @Before
+    public void setup() throws DAOException {
+        super.setup(getFactory().getCalendarAccess());
+    }
+    
+    @Override
+    protected CalendarLinkage create(long pkNumber, String pkSalt, int objNumber) {
+        return new CalendarLinkage(pkSalt + pkNumber, "userId" + objNumber);
+    }
+}
