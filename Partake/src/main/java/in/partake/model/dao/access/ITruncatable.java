181bdf9808d6926ef80ea787feb8baac02f7eb0c
mayah <mayah@mayah.jp>
2012-01-11 08:29:56 +0000

    ITruncatable is deleted, and is merged to IAccess.

--- Partake/src/main/java/in/partake/model/dao/access/ITruncatable.java
+++
@@ -1,9 +0,0 @@
-package in.partake.model.dao.access;
-
-import in.partake.model.dao.DAOException;
-import in.partake.model.dao.PartakeConnection;
-
-public interface ITruncatable {
-    /** Use ONLY in unit tests.*/
-    public abstract void truncate(PartakeConnection con) throws DAOException;
-}
