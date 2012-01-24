aed68555ee2b14f16f24534069ccb114d87d1c50
mayah <mayah@mayah.jp>
2012-01-24 11:12:17 +0000

    TestData should have UUID id instread of arbitrary ID.

--- Partake/src/test/java/in/partake/model/fixture/TestDataProvider.java
+++ Partake/src/test/java/in/partake/model/fixture/TestDataProvider.java
@@ -1,23 +1,25 @@
 package in.partake.model.fixture;
 
+import java.util.UUID;
+
 import in.partake.model.dao.DAOException;
 import in.partake.model.dao.PartakeConnection;
 import in.partake.model.dao.PartakeDAOFactory;
 
 public abstract class TestDataProvider<T> {
-    public static final String INVALID_USER_ID = "invalidUserId";
-    public static final String USER_ID1 = "testUser1";
-    public static final String USER_ID2 = "testUser2";
-    public static final String USER_ID3 = "testUser3";
+    public static final String INVALID_USER_ID = new UUID(1, -1).toString();
+    public static final String USER_ID1 = new UUID(1, 1).toString();
+    public static final String USER_ID2 = new UUID(1, 2).toString();
+    public static final String USER_ID3 = new UUID(1, 3).toString();
 
-    public static final String INVALID_EVENT_ID = "invalidEventId";
-    public static final String EVENT_ID1 = "event1";
-    public static final String EVENT_ID2 = "event2";
-    public static final String EVENT_ID3 = "event3";
+    public static final String INVALID_EVENT_ID = new UUID(2, -1).toString();
+    public static final String EVENT_ID1 = new UUID(2, 1).toString();
+    public static final String EVENT_ID2 = new UUID(2, 2).toString();
+    public static final String EVENT_ID3 = new UUID(2, 3).toString();
 
-    public static final String EVENT_PRIVATE_ID1 = "event-private-id1";
-    public static final String EVENT_PRIVATE_ID2 = "event-private-id2";
-    public static final String EVENT_PRIVATE_ID3 = "event-private-id3";
+    public static final String EVENT_PRIVATE_ID1 = new UUID(2, 11).toString();
+    public static final String EVENT_PRIVATE_ID2 = new UUID(2, 12).toString();
+    public static final String EVENT_PRIVATE_ID3 = new UUID(2, 13).toString();
     
     public abstract T create();
     public abstract T create(long pkNumber, String pkSalt, int objNumber);
