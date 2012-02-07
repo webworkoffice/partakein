ebf96fa8d8c7a3d1b07402d4550c3b9a1c4a1751
mayah@mayah.jp <mayah@mayah.jp@35327b65-f2d5-03ad-2307-039ebe4172a5>
2012-02-07 13:50:04 +0000

    Adds PartakeException, which can contain UserErrorCode or ServerErrorCode.

---
+++ trunk/Partake/src/main/java/in/partake/base/PartakeException.java
@@ -0,0 +1,53 @@
+package in.partake.base;
+
+import in.partake.resource.ServerErrorCode;
+import in.partake.resource.UserErrorCode;
+
+/**
+ * An Exception which contains ServerErrorCode or UserErrorCode.
+ * Either ServerErrorCode or UserErrorCode should be included.
+ *
+ * @author shinyak
+ */
+public class PartakeException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    private ServerErrorCode serverErrorCode;
+    private UserErrorCode userErrorCode;
+    
+    public PartakeException(ServerErrorCode ec) {
+        super(ec.getReasonString());
+        this.serverErrorCode = ec;
+    }
+
+    public PartakeException(ServerErrorCode ec, Throwable t) {
+        super(ec.getReasonString(), t);
+        this.serverErrorCode = ec;
+    }
+    
+    public PartakeException(UserErrorCode ec) {
+        super(ec.getReasonString());
+        this.userErrorCode = ec;
+    }
+
+    public PartakeException(UserErrorCode ec, Throwable t) {
+        super(ec.getReasonString(), t);
+        this.userErrorCode = ec;
+    }
+
+    public boolean isServerError() {
+        return serverErrorCode != null;
+    }
+    
+    public ServerErrorCode getServerErrorCode() {
+        return serverErrorCode;
+    }
+    
+    public boolean isUserError() {
+        return userErrorCode != null;
+    }
+    
+    public UserErrorCode getUserErrorCode() {
+        return userErrorCode;
+    }
+}
