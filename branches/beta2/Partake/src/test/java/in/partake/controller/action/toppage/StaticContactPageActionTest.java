5b20a6639077c7a35d23377ba3d4b696db9b3709
mayah@mayah.jp <mayah@mayah.jp@35327b65-f2d5-03ad-2307-039ebe4172a5>
2012-03-05 11:05:04 +0000

    Bugfix for StaticContactPage and StaticTermOfUsePage tests.

--- branches/beta2/Partake/src/test/java/in/partake/controller/action/toppage/StaticContactPageActionTest.java
+++ branches/beta2/Partake/src/test/java/in/partake/controller/action/toppage/StaticContactPageActionTest.java
@@ -11,24 +11,24 @@ import com.opensymphony.xwork2.ActionProxy;
 public class StaticContactPageActionTest extends AbstractPartakeControllerTest {
     @Test
     public void testToExecute() throws Exception {
-        ActionProxy proxy = getActionProxy("/termofuse");
+        ActionProxy proxy = getActionProxy("/contact");
         loginAs(proxy, TestDataProvider.USER_ID1);
 
         proxy.execute();
         assertResultSuccess(proxy);
         
         StaticContactPageAction action = (StaticContactPageAction) proxy.getAction();
-        Assert.assertEquals("termofuse.jsp", action.getLocation());
+        Assert.assertEquals("contact.jsp", action.getLocation());
     }
 
     @Test
     public void testToExecuteWithoutLogin() throws Exception {
-        ActionProxy proxy = getActionProxy("/termofuse");
+        ActionProxy proxy = getActionProxy("/contact");
 
         proxy.execute();
         assertResultSuccess(proxy);
         
         StaticContactPageAction action = (StaticContactPageAction) proxy.getAction();
-        Assert.assertEquals("termofuse.jsp", action.getLocation());
+        Assert.assertEquals("contact.jsp", action.getLocation());
     }
 }
--- branches/beta2/Partake/src/test/java/in/partake/controller/action/toppage/StaticTermOfUsePageActionTest.java
+++ branches/beta2/Partake/src/test/java/in/partake/controller/action/toppage/StaticTermOfUsePageActionTest.java
@@ -11,24 +11,24 @@ import com.opensymphony.xwork2.ActionProxy;
 public class StaticTermOfUsePageActionTest extends AbstractPartakeControllerTest {
     @Test
     public void testToExecute() throws Exception {
-        ActionProxy proxy = getActionProxy("/contact");
+        ActionProxy proxy = getActionProxy("/termofuse");
         loginAs(proxy, TestDataProvider.USER_ID1);
 
         proxy.execute();
         assertResultSuccess(proxy);
-        
-        StaticContactPageAction action = (StaticContactPageAction) proxy.getAction();
-        Assert.assertEquals("contact.jsp", action.getLocation());
+
+        StaticTermOfUsePageAction action = (StaticTermOfUsePageAction) proxy.getAction();
+        Assert.assertEquals("termofuse.jsp", action.getLocation());
     }
 
     @Test
     public void testToExecuteWithoutLogin() throws Exception {
-        ActionProxy proxy = getActionProxy("/contact");
+        ActionProxy proxy = getActionProxy("/termofuse");
 
         proxy.execute();
         assertResultSuccess(proxy);
         
-        StaticContactPageAction action = (StaticContactPageAction) proxy.getAction();
-        Assert.assertEquals("contact.jsp", action.getLocation());
+        StaticTermOfUsePageAction action = (StaticTermOfUsePageAction) proxy.getAction();
+        Assert.assertEquals("termofuse.jsp", action.getLocation());
     }
 }
