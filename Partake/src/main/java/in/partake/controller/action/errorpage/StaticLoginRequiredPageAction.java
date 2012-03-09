b194503a723a36608cb6ffd1468b11c6fab37c7a
mayah <mayah@mayah.jp>
2012-03-09 21:24:42 +0000

    Added StaticErrorPage tests.

--- Partake/src/main/java/in/partake/controller/action/errorpage/StaticLoginRequiredPageAction.java
+++ Partake/src/main/java/in/partake/controller/action/errorpage/StaticLoginRequiredPageAction.java
@@ -1,12 +1,18 @@
 package in.partake.controller.action.errorpage;
 
 import in.partake.controller.action.AbstractPartakeAction;
+import in.partake.model.UserEx;
 import in.partake.model.dao.DAOException;
 
 public class StaticLoginRequiredPageAction extends AbstractPartakeAction {
     private static final long serialVersionUID = 1L;
     
     public String doExecute() throws DAOException {
+        // If a user already has logged in, redirect to the top page.
+        UserEx user = getLoginUser();
+        if (user != null)
+            return renderRedirect("/");
+        
         return render("error/loginRequired.jsp");
     }    
 }
---
+++ Partake/src/test/java/in/partake/controller/action/errorpage/StaticForbiddenPageTest.java
@@ -0,0 +1,27 @@
+package in.partake.controller.action.errorpage;
+
+import in.partake.controller.AbstractPartakeControllerTest;
+import in.partake.model.fixture.TestDataProvider;
+
+import org.junit.Test;
+
+import com.opensymphony.xwork2.ActionProxy;
+
+public class StaticForbiddenPageTest extends AbstractPartakeControllerTest {
+    @Test
+    public void testAccessWithLogin() throws Exception {
+        ActionProxy proxy = getActionProxy("/prohibited");
+        loginAs(proxy, TestDataProvider.USER_ID1);
+        
+        proxy.execute();
+        assertResultSuccess(proxy);
+   }
+
+    @Test
+    public void testAccessWithoutLogin() throws Exception {
+        ActionProxy proxy = getActionProxy("/prohibited");
+
+        proxy.execute();
+        assertResultSuccess(proxy);
+    }
+}
