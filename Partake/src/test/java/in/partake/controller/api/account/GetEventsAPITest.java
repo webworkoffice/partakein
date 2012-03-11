fb2445d655a461a3527c608dc415a30eb5914989
mayah <mayah@mayah.jp>
2012-03-11 15:56:35 +0000

    AccountAPITest is split into tests by API.

---
+++ Partake/src/test/java/in/partake/controller/api/account/GetEventsAPITest.java
@@ -0,0 +1,6 @@
+package in.partake.controller.api.account;
+
+import in.partake.controller.api.APIControllerTest;
+
+public class GetEventsAPITest extends APIControllerTest {
+}
---
+++ Partake/src/test/java/in/partake/controller/api/account/SetPreferenceAPITest.java
@@ -0,0 +1,78 @@
+package in.partake.controller.api.account;
+
+import in.partake.controller.api.APIControllerTest;
+import in.partake.model.daofacade.deprecated.DeprecatedUserDAOFacade;
+import in.partake.model.dto.UserPreference;
+import in.partake.model.fixture.TestDataProvider;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.opensymphony.xwork2.ActionProxy;
+
+public class SetPreferenceAPITest extends APIControllerTest {
+    @Test
+    public void testToSetPreferenceWithLogin() throws Exception {
+        ActionProxy proxy = getActionProxy("/api/account/setPreference");
+
+        UserPreference pref = DeprecatedUserDAOFacade.get().getUserPreference(TestDataProvider.DEFAULT_USER_ID);
+        Assert.assertEquals(true, pref.isProfilePublic());
+        Assert.assertEquals(true, pref.isReceivingTwitterMessage());
+        Assert.assertEquals(true, pref.tweetsAttendanceAutomatically());
+
+        loginAs(proxy, TestDataProvider.DEFAULT_USER_ID);
+
+        addValidSessionTokenToParameter(proxy);
+        addParameter(proxy, "profilePublic", "false");
+        addParameter(proxy, "receivingTwitterMessage", "false");
+        addParameter(proxy, "tweetingAttendanceAutomatically", "false");        
+        proxy.execute();
+
+        assertResultOK(proxy);
+
+        pref = DeprecatedUserDAOFacade.get().getUserPreference(TestDataProvider.DEFAULT_USER_ID);
+        Assert.assertEquals(false, pref.isProfilePublic());
+        Assert.assertEquals(false, pref.isReceivingTwitterMessage());
+        Assert.assertEquals(false, pref.tweetsAttendanceAutomatically());
+    }
+
+    @Test
+    public void testToSetPreferenceWithLoginWithoutArgument() throws Exception {
+        ActionProxy proxy = getActionProxy("/api/account/setPreference");
+
+        UserPreference pref = DeprecatedUserDAOFacade.get().getUserPreference(TestDataProvider.DEFAULT_USER_ID);
+        Assert.assertEquals(true, pref.isProfilePublic());
+        Assert.assertEquals(true, pref.isReceivingTwitterMessage());
+        Assert.assertEquals(true, pref.tweetsAttendanceAutomatically());
+
+        loginAs(proxy, TestDataProvider.DEFAULT_USER_ID);
+        addValidSessionTokenToParameter(proxy);
+        proxy.execute();
+
+        assertResultOK(proxy);
+
+        pref = DeprecatedUserDAOFacade.get().getUserPreference(TestDataProvider.DEFAULT_USER_ID);
+        Assert.assertEquals(true, pref.isProfilePublic());
+        Assert.assertEquals(true, pref.isReceivingTwitterMessage());
+        Assert.assertEquals(true, pref.tweetsAttendanceAutomatically());
+    }
+
+    @Test
+    public void testToSetPreferenceWithLoginWithInvalidSessionToken() throws Exception {
+        ActionProxy proxy = getActionProxy("/api/account/setPreference");
+
+        loginAs(proxy, TestDataProvider.DEFAULT_USER_ID);
+        addInvalidSessionTokenToParameter(proxy);
+
+        proxy.execute();
+        assertResultInvalid(proxy);
+    }
+
+    @Test
+    public void testToSetPreferenceWithoutLogin() throws Exception {
+        ActionProxy proxy = getActionProxy("/api/account/setPreference");
+
+        proxy.execute();
+        assertResultLoginRequired(proxy);
+    }  
+}
