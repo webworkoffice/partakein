8b2aea9161b48b260387db036eaa2d00f695b7f7
mayah <mayah@mayah.jp>
2012-03-11 15:57:17 +0000

    user/GetAPI is transactionalized.

--- Partake/src/main/java/in/partake/controller/api/user/GetAPI.java
+++ Partake/src/main/java/in/partake/controller/api/user/GetAPI.java
@@ -4,8 +4,10 @@ import in.partake.base.PartakeException;
 import in.partake.controller.api.AbstractPartakeAPI;
 import in.partake.model.UserEx;
 import in.partake.model.dao.DAOException;
-import in.partake.model.daofacade.deprecated.DeprecatedUserDAOFacade;
-import net.sf.json.JSONObject;
+import in.partake.model.dao.PartakeConnection;
+import in.partake.model.dao.base.Transaction;
+import in.partake.model.daofacade.UserDAOFacade;
+import in.partake.resource.UserErrorCode;
 
 public class GetAPI extends AbstractPartakeAPI {
     private static final long serialVersionUID = 1L;
@@ -14,11 +16,23 @@ public class GetAPI extends AbstractPartakeAPI {
     protected String doExecute() throws DAOException, PartakeException {
         String userId = getValidUserIdParameter();
 
-        UserEx user = DeprecatedUserDAOFacade.get().getUserExById(userId);
+        UserEx user = new GetAPITransaction(userId).execute();
         if (user == null)
-            return renderNotFound();
-        
-        JSONObject obj = user.toSafeJSON();
-        return renderOK(obj);
+            return renderInvalid(UserErrorCode.INVALID_USER_ID);
+
+        return renderOK(user.toSafeJSON());
+    }
+}
+
+class GetAPITransaction extends Transaction<UserEx> {
+    private String userId;
+
+    public GetAPITransaction(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    protected UserEx doExecute(PartakeConnection con) throws DAOException, PartakeException {
+        return UserDAOFacade.getUserEx(con, userId); 
     }
 }
