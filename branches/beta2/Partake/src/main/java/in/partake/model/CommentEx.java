d40a1ca28d28c2725c8bc07e12f878cca68fadd7
mayah@mayah.jp <mayah@mayah.jp@35327b65-f2d5-03ad-2307-039ebe4172a5>
2012-03-25 06:48:19 +0000

    Buildfix.

--- branches/beta2/Partake/src/main/java/in/partake/model/CommentEx.java
+++ branches/beta2/Partake/src/main/java/in/partake/model/CommentEx.java
@@ -8,19 +8,13 @@ import in.partake.model.dto.Comment;
  *
  */
 public class CommentEx extends Comment {
-    private EventEx event;
     private UserEx user;
 
-    public CommentEx(Comment comment, EventEx event, UserEx user) {
+    public CommentEx(Comment comment, UserEx user) {
         super(comment);
-        this.event = event;
         this.user = user;
     }
 
-    public EventEx getEvent() {
-        return event;
-    }
-    
     public UserEx getUser() {
         return user;
     }
