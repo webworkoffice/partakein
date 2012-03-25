53f67acf1455dc563bc823b8ae6278825e6522ff
mayah <mayah@mayah.jp>
2012-03-25 10:03:18 +0000

    Added: RemovedAPITest.

--- Partake/src/main/java/in/partake/controller/base/permission/EventRemovePermission.java
+++ Partake/src/main/java/in/partake/controller/base/permission/EventRemovePermission.java
@@ -10,13 +10,10 @@ public class EventRemovePermission extends PartakePermission {
     public static boolean check(Event event, UserEx user) {
         assert event != null;
         assert user != null;
-        
+
         if (StringUtils.equals(event.getOwnerId(), user.getId()))
             return true;
-        
-        if (event.isManager(user.getTwitterLinkage().getScreenName()))
-            return true;
-        
+
         return false;
     }
 }
