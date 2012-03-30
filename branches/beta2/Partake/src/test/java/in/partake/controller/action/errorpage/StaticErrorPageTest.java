484189327327c9dcad5b2c9a112412d9934d2707
mayah@mayah.jp <mayah@mayah.jp@35327b65-f2d5-03ad-2307-039ebe4172a5>
2012-03-30 14:54:51 +0000

    Tests for StaticErrorPage and StaticInvalidPage

--- branches/beta2/Partake/src/test/java/in/partake/controller/action/errorpage/StaticErrorPageTest.java
+++ branches/beta2/Partake/src/test/java/in/partake/controller/action/errorpage/StaticErrorPageTest.java
@@ -1,7 +1,11 @@
 package in.partake.controller.action.errorpage;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
 import in.partake.controller.AbstractPartakeControllerTest;
 import in.partake.model.fixture.TestDataProvider;
+import in.partake.resource.ServerErrorCode;
 
 import org.junit.Test;
 
@@ -12,7 +16,7 @@ public class StaticErrorPageTest extends AbstractPartakeControllerTest {
     public void testAccessWithLogin() throws Exception {
         ActionProxy proxy = getActionProxy("/error");
         loginAs(proxy, TestDataProvider.DEFAULT_USER_ID);
-        
+
         proxy.execute();
         assertResultSuccess(proxy);
    }
@@ -24,4 +28,28 @@ public class StaticErrorPageTest extends AbstractPartakeControllerTest {
         proxy.execute();
         assertResultSuccess(proxy);
     }
+
+    @Test
+    public void testAccessWithErrorCode() throws Exception {
+        ActionProxy proxy = getActionProxy("/error");
+        addParameter(proxy, "errorCode", ServerErrorCode.INTENTIONAL_ERROR.getErrorCode());
+        proxy.execute();
+        assertResultSuccess(proxy);
+
+        StaticErrorPageAction action = (StaticErrorPageAction) proxy.getAction();
+
+        assertThat(action.getServerErrorCode(), is(ServerErrorCode.INTENTIONAL_ERROR));
+    }
+
+    @Test
+    public void testAccessWithInvalidErrorCode() throws Exception {
+        ActionProxy proxy = getActionProxy("/error");
+        addParameter(proxy, "errorCode", "hogehoge");
+        proxy.execute();
+        assertResultSuccess(proxy);
+
+        StaticErrorPageAction action = (StaticErrorPageAction) proxy.getAction();
+        assertThat(action.getServerErrorCode(), is(nullValue()));
+    }
+
 }
--- branches/beta2/Partake/src/test/java/in/partake/controller/action/errorpage/StaticInvalidPageTest.java
+++ branches/beta2/Partake/src/test/java/in/partake/controller/action/errorpage/StaticInvalidPageTest.java
@@ -1,7 +1,11 @@
 package in.partake.controller.action.errorpage;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
 import in.partake.controller.AbstractPartakeControllerTest;
 import in.partake.model.fixture.TestDataProvider;
+import in.partake.resource.UserErrorCode;
 
 import org.junit.Test;
 
@@ -12,7 +16,7 @@ public class StaticInvalidPageTest extends AbstractPartakeControllerTest {
     public void testAccessWithLogin() throws Exception {
         ActionProxy proxy = getActionProxy("/invalid");
         loginAs(proxy, TestDataProvider.DEFAULT_USER_ID);
-        
+
         proxy.execute();
         assertResultSuccess(proxy);
    }
@@ -23,5 +27,28 @@ public class StaticInvalidPageTest extends AbstractPartakeControllerTest {
 
         proxy.execute();
         assertResultSuccess(proxy);
+    }
+
+    @Test
+    public void testAccessWithErrorCode() throws Exception {
+        ActionProxy proxy = getActionProxy("/error");
+        addParameter(proxy, "errorCode", UserErrorCode.INTENTIONAL_USER_ERROR.getErrorCode());
+        proxy.execute();
+        assertResultSuccess(proxy);
+
+        StaticInvalidPageAction action = (StaticInvalidPageAction) proxy.getAction();
+
+        assertThat(action.getUserErrorCode(), is(UserErrorCode.INTENTIONAL_USER_ERROR));
+    }
+
+    @Test
+    public void testAccessWithInvalidErrorCode() throws Exception {
+        ActionProxy proxy = getActionProxy("/error");
+        addParameter(proxy, "errorCode", "hogehoge");
+        proxy.execute();
+        assertResultSuccess(proxy);
+
+        StaticInvalidPageAction action = (StaticInvalidPageAction) proxy.getAction();
+        assertThat(action.getUserErrorCode(), is(nullValue()));
     }
 }
