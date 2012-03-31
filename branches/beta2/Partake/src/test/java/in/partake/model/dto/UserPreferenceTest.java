f7727dba3c4008e3a86d215d2c729b57e300003a
mayah@mayah.jp <mayah@mayah.jp@35327b65-f2d5-03ad-2307-039ebe4172a5>
2012-03-31 02:01:25 +0000

    Tests for UserPreference.

--- branches/beta2/Partake/src/test/java/in/partake/model/dto/UserPreferenceTest.java
+++ branches/beta2/Partake/src/test/java/in/partake/model/dto/UserPreferenceTest.java
@@ -1,5 +1,7 @@
 package in.partake.model.dto;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,19 +12,23 @@ public class UserPreferenceTest extends AbstractPartakeModelTest<UserPreference>
     @Before
     public void createSamples() {
         samples = new UserPreference[] {
-            new UserPreference("id1", true, true, false),
-            new UserPreference("id2", true, true, true),
-            new UserPreference("id3", false, false, false),
-            new UserPreference("id4", false, true, false),
+            new UserPreference("id1", false, false, false),
+            new UserPreference("id2", true, false, true),
+            new UserPreference("id3", false, true, false),
+            new UserPreference("id4", true, true, false),
+            new UserPreference("id5", false, false, true),
+            new UserPreference("id6", true, false, true),
+            new UserPreference("id7", false, true, true),
+            new UserPreference("id8", true, true, true),
         };
     }
-    
+
     @Test
     public void testCopyConstructor() {
         for (UserPreference source : samples) {
             Assert.assertEquals(source, new UserPreference(source));
         }
-        
+
         for (UserPreference lhs : samples) {
             for (UserPreference rhs : samples) {
                 if (lhs == rhs) { continue; }
@@ -31,9 +37,21 @@ public class UserPreferenceTest extends AbstractPartakeModelTest<UserPreference>
         }
     }
 
-	@Override
-	protected UserPreference createModel() {
-		return new UserPreference();
-	}
+    @Test
+    public void testEquals() {
+        UserPreference pref = samples[0];
+        assertThat(pref.equals(null), is(false));
+
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                assertThat(samples[i].equals(samples[j]), is(i == j));
+            }
+        }
+    }
+
+    @Override
+    protected UserPreference createModel() {
+        return new UserPreference();
+    }
 
 }
